807fe7c98e7a2e9b2bbea3ebdd92c7eaadabf39d
Adam Kong <akong@cawst.org>
2026-01-21 10:34:54 -0700

    setup rewrites for child next

--- examples/child-next/next.config.ts
+++ examples/child-next/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/child-next",
+  assetPrefix: "/child-next/",
 };
 
 export default nextConfig;
--- examples/host-app/next.config.ts
+++ examples/host-app/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: "/child-next/:path*",
+        destination: "http://localhost:3001/child-next/:path*", // Proxy to Child Next.js app
+      },
+      {
+        source: "/child-nuxt/:path*",
+        destination: "http://localhost:3002/child-nuxt/:path*", // Proxy to Child Nuxt.js app
+      },
+    ];
+  },
 };
 
 export default nextConfig;
