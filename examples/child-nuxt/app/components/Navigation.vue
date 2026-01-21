8f3c764847f176e78c08a18a51e1e1fca7879f5c
Adam Kong <akong@cawst.org>
2026-01-21 13:52:26 -0700

    added navigation to child nuxt

---
+++ examples/child-nuxt/app/components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-16 px-4 py-2">
+    <nav class="grid grid-cols-3 gap 4 items-center h-full">
+      <h2>Navigation Component</h2>
+      <ul class="flex justify-center gap-6">
+        <li v-for="item in navigationItems" :key="item.link">
+          <a :href="item.link">{{ item.name }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup lang="tsx">
+const navigationItems = [
+  { name: "Home", link: "/" },
+  { name: "Child-Next", link: "/child-next" },
+  { name: "Child-Nuxt", link: "/child-nuxt/home" },
+];
+</script>
