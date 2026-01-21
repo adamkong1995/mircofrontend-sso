134152c5ab77b92b68921645e37942fa881da25f
Adam Kong <akong@cawst.org>
2026-01-21 10:17:50 -0700

    setup tailwind for child nuxt app

--- examples/child-nuxt/app/layouts/default.vue
+++ examples/child-nuxt/app/layouts/default.vue
@@ -1,22 +1,7 @@
 <template>
-  <div class="default">
+  <div>
     <slot />
   </div>
 </template>
 
-<style scoped>
-.default {
-  border: 1px solid #b9e39e;
-  padding: 1rem;
-  position: relative;
-  background-color: #b8e39e87;
-}
-.default::before {
-  content: "layouts/default.vue";
-  position: absolute;
-  top: 2px;
-  left: 5px;
-  color: #68b538;
-  font-family: font-sans;
-}
-</style>
+<style scoped></style>
--- examples/child-nuxt/app/pages/index.vue
+++ examples/child-nuxt/app/pages/index.vue
@@ -2,6 +2,12 @@
   <div
     className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"
   >
-    <div>Child Next App</div>
+    <div className="text-white body">Child Nuxt App</div>
   </div>
 </template>
+
+<style lang="css" scoped>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
