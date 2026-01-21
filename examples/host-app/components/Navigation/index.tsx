f7e39f3e20450d8b4e700d26ddf283dd0923f5f5
Adam Kong <akong@cawst.org>
2026-01-21 10:24:03 -0700

    added basic navigation

---
+++ examples/host-app/components/Navigation/index.tsx
@@ -0,0 +1,24 @@
+const navigationButtons = [
+  { name: "Home", link: "/" },
+  { name: "Child-Next", link: "/child-next" },
+  { name: "Child-Nuxt", link: "/child-nuxt" },
+];
+
+const Navigation = () => {
+  return (
+    <div className="h-16 px-4 py-2">
+      <div className="grid grid-cols-3 gap-4 flex items-center h-full">
+        <h2>Navigation Component</h2>
+        <div className="flex justify-center gap-6">
+          {navigationButtons.map((button) => (
+            <a key={button.name} href={button.link}>
+              {button.name}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
