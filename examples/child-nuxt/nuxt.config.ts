a69891b1ec62834beaa6666d00e3311fd61c475e
Adam Kong <akong@cawst.org>
2026-01-21 10:53:32 -0700

    re-setup tailwind css for child nuxt app

--- examples/child-nuxt/nuxt.config.ts
+++ examples/child-nuxt/nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
   app: {
     baseURL: "/child-nuxt",
   },
+  css: ["./app/assets/css/tailwind.css"],
+  vite: { plugins: [tailwindcss()] },
   // tailwindcss: {
   //   cssPath: "~/assets/css/tailwind.css",
   //   configPath: "tailwind.config.js",
