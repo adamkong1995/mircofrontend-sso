ac9e328e90be70905208de909549613fb2d8aa3c
Adam Kong <akong@cawst.org>
2026-01-28 14:12:06 -0700

    setup auth0

--- examples/host-app/components/Navigation/index.tsx
+++ examples/host-app/components/Navigation/index.tsx
@@ -1,10 +1,18 @@
+import LoginButton from "../Login";
+import LogoutButton from "../Logout";
+import Profile from "../Profile";
+import { auth0 } from "@/services/auth0/client";
+
 const navigationButtons = [
   { name: "Home", link: "/" },
   { name: "Child-Next", link: "/child-next" },
   { name: "Child-Nuxt", link: "/child-nuxt/home" },
 ];
 
-const Navigation = () => {
+const Navigation = async () => {
+  const session = await auth0.getSession();
+  const user = session?.user;
+
   return (
     <div className="h-16 px-4 py-2">
       <div className="grid grid-cols-3 gap-4 items-center h-full">
@@ -15,6 +23,11 @@ const Navigation = () => {
               {button.name}
             </a>
           ))}
+        </div>
+        <div className="flex justify-end items-center gap-4">
+          <Profile />
+          {!user && <LoginButton />}
+          {user && <LogoutButton />}
         </div>
       </div>
     </div>
